2701a8eed2d3f1f9a3ebd0b4da84acdc23651957
mraba <mrabaneda18@gmail.com>
2023-05-30 22:25:23 +0200

    estilos webscrapping + blog terminado

--- marvelintuneapp/views.py
+++ marvelintuneapp/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.template.response import TemplateResponse
-from django.template.context import Context
 # Imports para usar BeautifulSoup
 import requests
 from bs4 import BeautifulSoup
