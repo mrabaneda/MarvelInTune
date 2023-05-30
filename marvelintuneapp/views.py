3c73921c520c1448a943bee0bb2051c33efe2f4f
mraba <mrabaneda18@gmail.com>
2023-05-30 22:05:11 +0200

    Scrapping Articulos de web marvel

--- marvelintuneapp/views.py
+++ marvelintuneapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
+from django.template.context import Context
+# Imports para usar BeautifulSoup
+import requests
+from bs4 import BeautifulSoup
 # Create your views here.
 
 def index(request):
@@ -10,4 +15,18 @@ def canciones(request):
     return render(request,'canciones.html')
 
 def blog(request):
-    return render(request,'blog.html')
+    url = 'https://marvelblog.com/'  # Reemplaza con la URL de la página que deseas raspar
+    response = requests.get(url)
+    html = response.content
+
+    # Crea un objeto BeautifulSoup con el HTML
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Encuentra todas las etiquetas <article>
+    articles = soup.find_all('article')
+
+    # Construye una lista con los artículos convertidos a cadenas de texto
+    article_strings = [str(article) for article in articles]
+
+    # Renderiza el template con el contenido HTML de los artículos
+    return TemplateResponse(request, 'blog.html', {'articles': article_strings})
